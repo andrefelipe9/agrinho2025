58a52ab4ad91a30fac81183e4f21d20fbfb1b0dc
andrefelipe9 <andre.scheiffer.schneider@escola.pr.gov.br>
2025-06-12 11:17:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("lightblue");
+
 fill("#663300")
   rect(0,300,400,200);
 fill("#009900")
@@ -13,15 +14,22 @@ fill("yellow");
 fill("#cc6600")
   rect(350,170,20,120);
 fill("lightgreen");
-  circle(350,170,60)
-fill("lightgreen");
-  circle(380,170,60)
-fill("lightgreen");
-  circle(330,160,60)
-fill("lightgreen");
-  circle(340,150,60)
-fill("lightgreen");
-  circle(350,170,60)
+  circle(360,170,100)
+fill("red");
+  circle(350,170,10)
+fill("red");
+  circle(330,190,10)
+fill("red");
+  circle(360,130,10)
+fill("red");
+  circle(380,187,10)
+fill("red");
+  circle(375,150,10)
+fill("red");
+  circle(320,160,10)
+fill("red");
+  circle(355,200,10)
+
 
 
 
